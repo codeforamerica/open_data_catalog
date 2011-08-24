e140001d8a452452840e47394eff77ec3ac8ad3f
Zach Williams <hey@zachwill.com>
2011-08-23 17:05:04 -0700

    Modified the url patterns

--- urls.py
+++ urls.py
@@ -1,7 +1,8 @@
 """URL patterns for the website."""
 
-from django.conf.urls.defaults import patterns, include, url
 from django.contrib import admin
+from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import redirect_to
 
 admin.autodiscover()
 
@@ -14,14 +15,14 @@ urlpatterns = patterns('data_catalog.views',
     url(r'^submit/app/$', 'submit_app'),
     url(r'^submit/idea/$', 'submit_idea'),
     url(r'^submit/data/$', 'submit_data'),
-    url(r'^(?P<name>\w+)\.txt$', 'send_text_file'),
-    url(r'^accounts/', include('registration.urls')),
 )
 
 
 urlpatterns += patterns('',
-    # Uncomment the admin/doc line below to enable admin documentation:
-    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-    # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^(?P<name>\w+)\.txt$', 'send_text_file'),
+    url(r'^login/$', 'django.contrib.auth.views.login'),
+    url(r'^logout/$', 'django.contrib.auth.views.logout_then_login'),
+    url(r'^accounts/login/$', redirect_to, {'url': '/login/'}),
+    url(r'^accounts/', include('registration.urls')),
 )
