18db26a6d41f51c211be60fa1b9f43381ae0a104
Zach Williams <hey@zachwill.com>
2011-09-01 17:16:49 -0700

    Fixed indentation

--- causes/views.py
+++ causes/views.py
@@ -17,10 +17,10 @@ def causes(request):
 
 
 def individual_cause(request, slug):
-	"""Render a specific cause."""
-	specific_cause = get_object_or_404(Cause, slug=slug)
-	context = {'cause': specific_cause}
-	return render_response(request, 'individual_cause.html', context)
+    """Render a specific cause."""
+    specific_cause = get_object_or_404(Cause, slug=slug)
+    context = {'cause': specific_cause}
+    return render_response(request, 'individual_cause.html', context)
 
 
 def send_text_file(request, name):
