202697f5aa3104d35228d73785d5c451d78acf4d
Zach Williams <hey@zachwill.com>
2011-09-08 17:36:05 -0700

    Where in the world did the tabs come from?

--- static/js/main.js
+++ static/js/main.js
@@ -1,26 +1,26 @@
 (function(window, $){
 
-	// Set up a namespace.
-	var ns = {
-		dom: {},
-		event: {},
+    // Set up a namespace.
+    var ns = {
+        dom: {},
+        event: {},
         cache: {}
-	};
+    };
 
-	ns.event.searchFocus = function(e) {
-		// Event that takes when the search bar becomes focused.
-		var self = $(this),
-			time = 200,
-			animation;
+    ns.event.searchFocus = function(e) {
+        // Event that takes when the search bar becomes focused.
+        var self = $(this),
+            time = 200,
+            animation;
 
-		if (e.type == 'focusin') {
-			//animation = { width: '+=25%' };
-		} else {
-			//animation = { width: '100%' };
-		}
+        if (e.type == 'focusin') {
+            //animation = { width: '+=25%' };
+        } else {
+            //animation = { width: '100%' };
+        }
 
-		self.animate(animation, time);
-	}
+        self.animate(animation, time);
+    }
 
     ns.event.ajaxAutocomplete = function(request, response) {
         // Autocomplete functionality for searching.
@@ -55,35 +55,35 @@
         });
     }
 
-	ns.dom.search = function(e) {
-		// Bind the searchFocus function and add autocomplete.
-		var search = $('input.search_bar');
-		search.autocomplete({
+    ns.dom.search = function(e) {
+        // Bind the searchFocus function and add autocomplete.
+        var search = $('input.search_bar');
+        search.autocomplete({
             source: ns.event.ajaxAutocomplete,
             select: function(e) {
                 var form = $('form');
                 form.submit();
             }
-		});
-	}
+        });
+    }
 
-	ns.dom.init = function(){
-		// Initalize function for DOM functionality.
-		var dom = ns.dom;
-		dom.search();
-	}
+    ns.dom.init = function(){
+        // Initalize function for DOM functionality.
+        var dom = ns.dom;
+        dom.search();
+    }
 
-	ns.main = (function(){
-		// Anonymous function that acts much like C's ``int main``.
-		var dom = ns.dom;
-		dom.init();
+    ns.main = (function(){
+        // Anonymous function that acts much like C's ``int main``.
+        var dom = ns.dom;
+        dom.init();
 
-		// Bind the namespace function to the window.
-		window.ns = ns;
+        // Bind the namespace function to the window.
+        window.ns = ns;
 
-		// Just making sure that it executed.
-		return true;
-	})();
+        // Just making sure that it executed.
+        return true;
+    })();
 
 
 })(window, jQuery);
