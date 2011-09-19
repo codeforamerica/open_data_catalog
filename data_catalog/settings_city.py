8a1b81b450584b2511fce854ce0f5658967058c8
Zach Williams <hey@zachwill.com>
2011-09-19 14:47:18 -0700

    Restructure the __all__ variable

--- data_catalog/settings_city.py
+++ data_catalog/settings_city.py
@@ -11,7 +11,6 @@ ACCOUNT_ACTIVATION_DAYS = 7
 DEFAULT_FROM_EMAIL = 'admin@' + CATALOG_URL
 LOGIN_REDIRECT_URL = '/'
 
-
 # This should work both locally and on DotCloud.
 if os.path.exists('/home/dotcloud/current'):
     DB_PATH = '/home/dotcloud/'
@@ -19,5 +18,7 @@ else:
     DB_PATH = ''
 
 
-__all__ = [CITY_NAME, CATALOG_URL, ACCOUNT_ACTIVATION_DAYS, DEFAULT_FROM_EMAIL,
-           LOGIN_REDIRECT_URL, DB_PATH]
+__all__ = [
+    CITY_NAME, CATALOG_URL, ACCOUNT_ACTIVATION_DAYS, DEFAULT_FROM_EMAIL,
+    LOGIN_REDIRECT_URL, DB_PATH
+]
