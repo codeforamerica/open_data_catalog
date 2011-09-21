ed26f57f755031e5bff6fb15bf42527209940864
Zach Williams <hey@zachwill.com>
2011-09-21 14:53:50 -0700

    Push bootstrap test page

--- urls.py
+++ urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.defaults import patterns, include, url
 from django.views.generic.simple import redirect_to
+from django.views.generic.simple import direct_to_template
 
 admin.autodiscover()
 
@@ -32,6 +33,12 @@ urlpatterns += patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('registration.urls')),
     url(r'^accounts/', include('registration.urls')),
+)
+
+
+# For pivot/testing...
+urlpatterns += patterns('',
+    url(r'^testing', direct_to_template, {'template': 'testing.html'}),
 )
 
 
