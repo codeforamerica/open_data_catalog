4f7d89e3fe41041c40d953d0af17151e52e234ee
Zach Williams <hey@zachwill.com>
2011-09-22 10:25:19 -0700

    General improvements to the top navigation

--- static/js/bootstrap.main.js
+++ static/js/bootstrap.main.js
@@ -1,8 +1,18 @@
 (function(window, $) {
+
+    // Search bar functionality.
     var search = $('.magnifying-glass').find('a');
     search.click(function(e) {
         var form = $('#search-bar');
         e.preventDefault();
         form.trigger('submit');
     });
+
+
+    // Prevent bubbling on dropdown menu.
+    var menu = $('.menu-dropdown').find('*');
+    menu.click(function(e) {
+        e.stopPropagation();
+    });
+
 })(window, jQuery);
