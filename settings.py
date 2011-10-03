4baac428a01667cadfc32bc20587f54961f63fdf
Zach Williams <hey@zachwill.com>
2011-10-03 10:49:25 -0700

    Update database settings for DotCloud

--- settings.py
+++ settings.py
@@ -31,16 +31,12 @@ TEMPLATE_CONTEXT_PROCESSORS += (
     'data_catalog.context_processors.settings_context',
 )
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'test.db',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    }
-}
+# Database settings below...
+DATABASE_ENGINE = 'django.db.backends.sqlite3'
+if os.path.exists('/home/dotcloud'):
+    DATABASE_NAME = '/home/dotcloud/test.db'
+else:
+    DATABASE_NAME = 'test.db'
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
