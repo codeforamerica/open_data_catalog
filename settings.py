e488013b4122f34a44e134f591bbb1615ffa1e47
Zach Williams <hey@zachwill.com>
2011-10-12 13:57:38 -0700

    Add static admin media

--- settings.py
+++ settings.py
@@ -75,7 +75,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/home/dotcloud'
+STATIC_ROOT = '/home/dotcloud/static'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
